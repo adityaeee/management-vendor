49e93974693b3b12e1d5d8aa2f227b9b4db142f9
adityae <rifkyaditya966@gmail.com>
2024-11-17 15:54:48 +0700

    Create Error Controller for error handler

---
+++ management/src/main/java/com/aditya/management/controller/ErrorController.java
@@ -0,0 +1,50 @@
+package com.aditya.management.controller;
+
+import com.aditya.management.DTO.response.BaseResponse;
+import com.aditya.management.DTO.response.CommonResponse;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestControllerAdvice
+public class ErrorController {
+
+    @ExceptionHandler({ResponseStatusException.class})
+    public ResponseEntity<BaseResponse> responseResponseStatusEntityException (ResponseStatusException exception) {
+        CommonResponse<?> response = CommonResponse.builder()
+                .statusCode(exception.getStatusCode().value())
+                .message(exception.getMessage())
+                .build();
+
+        return ResponseEntity
+                .status(exception.getStatusCode())
+                .body(response);
+    }
+
+    @ExceptionHandler({DataIntegrityViolationException.class})
+    public ResponseEntity<BaseResponse> dataIntegrityViolationException (DataIntegrityViolationException e) {
+        CommonResponse.CommonResponseBuilder<Object> builder =CommonResponse.builder();
+        HttpStatus httpStatus;
+
+        if(e.getMessage().contains("foreign key constraint")) {
+            builder.statusCode(HttpStatus.BAD_REQUEST.value());
+            builder.message("cannot delete data because other table depend on it");
+            httpStatus = HttpStatus.BAD_REQUEST;
+        } else if(e.getMessage().contains("unique constraint") || e.getMessage().contains("Duplicate entry")) {
+            builder.statusCode(HttpStatus.CONFLICT.value());
+            builder.message("Data already exist");
+            httpStatus = HttpStatus.CONFLICT;
+        } else {
+            builder.statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            builder.message("Internal Server Error");
+            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        
+        return ResponseEntity
+                .status(httpStatus)
+                .body(builder.build());
+    }
+}
